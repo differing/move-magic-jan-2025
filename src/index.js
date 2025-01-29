5dedc847db53480b39e969b9fc3cb1d87759d2e2
YulianChenashki <differing@abv.bg>
2025-01-29 10:26:02 +0200

    install and setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('IT works');
+//console.log('IT works');
+//sled kato sme vaveli komnadata npm i express
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res)=> {
+    res.send('It work!');
+});
+
+app.listen(5000, ()=> console.log('Server is listening on http:/localhost:5000...'));
